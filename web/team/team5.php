922a0f75ae180584ae38a191c4a7211d33b15dbe
Timothy Harris <subspace31@gmail.com>
2017-05-16 22:58:47 -0400

    changed to connect to heroku postgress.

--- web/team/team5.php
+++ web/team/team5.php
@@ -1,18 +1,30 @@
 <?php
+// default Heroku Postgres configuration URL
+$dbUrl = getenv('DATABASE_URL');
 
-$servername = "localhost";
-$username = "postgress";
-$password = "";
+if (empty($dbUrl)) {
+    // example localhost configuration URL with postgres username and a database called cs313db
+    $dbUrl = "postgres://postgres:password@localhost:5432/cs313db";
+}
+
+$dbopts = parse_url($dbUrl);
+
+print "<p>$dbUrl</p>\n\n";
+
+$dbHost = $dbopts["host"];
+$dbPort = $dbopts["port"];
+$dbUser = $dbopts["user"];
+$dbPassword = $dbopts["pass"];
+$dbName = ltrim($dbopts["path"],'/');
+
+print "<p>pgsql:host=$dbHost;port=$dbPort;dbname=$dbName</p>\n\n";
 
 try {
-    $conn = new PDO("mysql:host=$127.0.0.1;dbname=public", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 }
-catch(PDOException $e)
-{
-    echo "Connection failed: " . $e->getMessage();
+catch (PDOException $ex) {
+    print "<p>error: $ex->getMessage() </p>\n\n";
+    die();
 }
 ?>
 
